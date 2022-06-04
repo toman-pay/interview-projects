4576502b2b7e4faa843a42cc883df527d44c807f
iamsaeedfadaei <saeed.fadaei1374@gmail.com>
2022-06-04 16:38:31 +0430

    adding configurationg to use python-dotenv for database configuration

--- escrow_project/escrow_project/settings.py
+++ escrow_project/escrow_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'escrow_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('POSTGRES_DB', default=PROJECT_NAME),
+        'USER': os.getenv('POSTGRES_USER', default='postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
+        'HOST': os.getenv('POSTGRES_HOST', default='db'),
+        'PORT': int(os.getenv('POSTGRES_PORT', default='5432')),
+    },
 }
 
 
