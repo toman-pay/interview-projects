7d9e887090272d263e3f02dd1a731c7dfd2f0649
Mohammad Moayeri <moayeri@bamdadblockchain.com>
2022-06-05 00:29:47 +0430

    edited utils

--- src/products/utils.py
+++ src/products/utils.py
@@ -2,12 +2,13 @@ from rest_framework.exceptions import ValidationError
 from os import path
 from django.utils import timezone
 from django.conf import settings
+from .messages import Messages
 
 
 def is_image(ext):
     support_ext = settings.SUPPORTED_IMAGE_FORMATS
     if ext.lower() not in support_ext:
-        raise ValidationError('unknown file format')
+        raise ValidationError({'error' : Messages.INVALID_FORMAT.value})
     return True
 
 def product_image_path(instance, filename):
