fa4d07dba57a37746ccd39086436bc0b1ac7adb3
Mohammad Moayeri <moayeri@bamdadblockchain.com>
2022-06-04 23:55:31 +0430

    added swagger version

--- src/common/swaggers.py
+++ src/common/swaggers.py
@@ -1,11 +1,13 @@
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-
+from drf_yasg.inspectors import SwaggerAutoSchema
+from rest_framework.request import is_form_media_type
 
 schema_view = get_schema_view(
    openapi.Info(
       title="excrow APIS",
+      default_version='v1',
    ),
    public=True,
    permission_classes=[permissions.AllowAny],
