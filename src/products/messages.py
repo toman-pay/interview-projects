b4f58a3d178d391940cbc7cdb57ebfcaa5896f50
Mohammad Moayeri <moayeri@bamdadblockchain.com>
2022-06-05 14:10:48 +0430

    updated messages

--- src/products/messages.py
+++ src/products/messages.py
@@ -4,3 +4,5 @@ from django.utils.translation import gettext_lazy as _
 
 class Messages(Enum):
     INVALID_FORMAT = _("unknown file format")
+    IMAGE_SIZE = _("image size should not be larger than 2MB")
+    MIN_VALUE = _('Ensure this value is greater than {min_value}.')
