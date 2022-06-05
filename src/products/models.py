29e37806f99778373116789603174ff87e77ba24
Mohammad Moayeri <moayeri@bamdadblockchain.com>
2022-06-05 14:12:57 +0430

    renamed images field

--- src/products/models.py
+++ src/products/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from common.basemodels import BaseModel
 from django.utils.translation import gettext_lazy as _
 from .utils import product_image_path
+from django.conf import settings 
 
 
 class ProductModel(BaseModel):
@@ -10,7 +11,9 @@ class ProductModel(BaseModel):
     """
     title = models.CharField(_("title"), max_length=50)
     description = models.TextField(_("description"))
-    price = models.DecimalField(_("price"), max_digits=5, decimal_places=2)
+    price = models.DecimalField(
+        _("price"), max_digits=settings.MAX_DIGITS_PRICE, decimal_places=settings.DECIMAL_PLACES
+    )
 
     def __str__(self):
         return self.title
@@ -21,7 +24,7 @@ class ProductImageModel(BaseModel):
     This model is used to store product photos
     """
     product = models.ForeignKey(ProductModel, verbose_name=_("product"), on_delete=models.CASCADE)
-    images = models.ImageField(_("image"), upload_to=product_image_path, null=True , blank=True)
+    image = models.ImageField(_("image"), upload_to=product_image_path, null=True , blank=True)
 
     def __str__(self):
-        return self.product.title
+        return self.image.name
