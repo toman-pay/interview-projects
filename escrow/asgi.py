59477f62fb20a0d884a48fb4eae865a250051e45
M.Mortaz <Mortaz.Mehdi@gmail.com>
2022-07-09 18:10:40 +0430

    Add development settings as default settings

--- escrow/asgi.py
+++ escrow/asgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'escrow.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'escrow.settings.settings_dev')
 
 application = get_asgi_application()
--- escrow/wsgi.py
+++ escrow/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'escrow.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'escrow.settings.settings_dev')
 
 application = get_wsgi_application()
