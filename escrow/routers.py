c9f314887f74894c2d67ce66039e6b32b3b2132c
M.Mortaz <Mortaz.Mehdi@gmail.com>
2022-07-10 19:29:12 +0430

    Add Product app router to project router

--- escrow/routers.py
+++ escrow/routers.py
@@ -1,7 +1,9 @@
 from rest_framework import routers
 from apps.user.routers import router_v1 as user_router_v1
 from apps.file.routers import router_v1 as file_router_v1
+from apps.product.routers import router_v1 as product_router_v1
 
 app_router_v1 = routers.DefaultRouter()
 app_router_v1.registry.extend(user_router_v1.registry)
 app_router_v1.registry.extend(file_router_v1.registry)
+app_router_v1.registry.extend(product_router_v1.registry)
